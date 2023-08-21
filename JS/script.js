b9a49e9b8c290f1aeb5caffc04e864b74971baf2
Jared Nash <nash_jar000@byui.edu>
2023-08-21 12:56:26 -0700

    changed duration of header and target=blank

--- JS/script.js
+++ JS/script.js
@@ -14,21 +14,21 @@ function typeCode() {
     if (charIndex < codeText[lineIndex].length) {
       typedText.textContent += codeText[lineIndex].charAt(charIndex);
       charIndex++;
-      setTimeout(typeCode, 50);
+      setTimeout(typeCode, 60);
     } else {
       lineIndex++;
       charIndex = 0;
-      setTimeout(clearText, 1000); // Longer break here
+      setTimeout(clearText, 1500); // Longer break here
     }
   } else {
     cursor.style.display = 'none';
-    setTimeout(startAnimation, 1000); // Longer break here
+    setTimeout(startAnimation, 1500); // Longer break here
   }
 }
 
 function clearText() {
   typedText.textContent = '';
-  setTimeout(typeCode, 500);
+  setTimeout(typeCode, 1500);
 }
 
 function startAnimation() {
