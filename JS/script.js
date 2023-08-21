7811432e90aede74706bd9145d0e4f9640afdd98
Jared Nash <nash_jar000@byui.edu>
2023-08-21 12:18:53 -0700

    first commit

---
+++ JS/script.js
@@ -0,0 +1,42 @@
+const typedText = document.getElementById('typed-text');
+const cursor = document.getElementById('cursor');
+const codeText = [
+  'const name = "Jared Nash";',
+  'console.log(`Hello, my name is ${name}.`);',
+  'Hello, my name is Jared Nash',
+];
+
+let lineIndex = 0;
+let charIndex = 0;
+
+function typeCode() {
+  if (lineIndex < codeText.length) {
+    if (charIndex < codeText[lineIndex].length) {
+      typedText.textContent += codeText[lineIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(typeCode, 50);
+    } else {
+      lineIndex++;
+      charIndex = 0;
+      setTimeout(clearText, 1000); // Longer break here
+    }
+  } else {
+    cursor.style.display = 'none';
+    setTimeout(startAnimation, 1000); // Longer break here
+  }
+}
+
+function clearText() {
+  typedText.textContent = '';
+  setTimeout(typeCode, 500);
+}
+
+function startAnimation() {
+  typedText.textContent = '';
+  lineIndex = 0;
+  charIndex = 0;
+  cursor.style.display = 'inline-block';
+  typeCode();
+}
+
+startAnimation();
